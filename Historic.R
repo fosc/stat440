09508def4473c3bbee5ca16b8573cd5158c55d65
Frank Cento <foscento@gmail.com>
2018-04-06 09:43:15 -0400

    changes to getWindow. Portfolio_timeseries function added. This function will calculate all rebalancing of the portfolio

--- Historic.R
+++ Historic.R
@@ -13,16 +13,15 @@ S<- as.matrix(subset(snp500, select = - c(Date, VIX)))
 #' @param trajectLength we ensure that there is enough room for a trajectory of length trajectLength between 
 #' the end of the window and the end of the data in matrix S.
 #' @return Matrix of corresponding student residuals
-getWindow <- function(S, wLength, wOffset, trajectLength){
+getWindow <- function(S, wLength, wOffset){
   
   t_final = dim(S)[1]
   
   #we don't train on less than 2000 daily observations.
   if(wLength<2000) print("window length is below 2000. Fails to meet minimum data requirements.")
-  if(wLength+wOffset+trajectLength > t_final ) stop("cannot create window. Doing so would push timesteps beyond t_final.")
+  if(wLength+wOffset > t_final ) stop("cannot create window. Doing so would push timesteps beyond t_final.")
   if(wOffset < 0) stop("cannot have negative offset for window")
-  if(trajectLength < 1) stop("trajectLenght < 1. No sense in predicting anything before tomorrow.")
-  
+
   return(S[(1+wOffset):(wOffset+wLength),])
 }
 
@@ -33,11 +32,33 @@ testS = matrix(c(1.00,2,3,4,5,6,7.101,8,9,11,12,13,14,15.999,16,17,18,19), ncol=
 #install.packages("testthat")
 require(testthat)
 
-expect_identical(getWindow(testS,wLength=3, wOffset=1, trajectLength=1 ), testS[2:4,] )
-expect_identical(getWindow(testS,wLength=3, wOffset=0, trajectLength=1 ), testS[1:3,] )
-expect_error(getWindow(testS,wLength=3, wOffset=0, trajectLength=4 ) )
-expect_error(getWindow(testS,wLength=3, wOffset=0, trajectLength=0 ) )
-expect_error(getWindow(testS,wLength=3, wOffset=-1, trajectLength=2 ) )
+expect_identical(getWindow(testS,wLength=3, wOffset=1 ), testS[2:4,] )
+expect_identical(getWindow(testS,wLength=3, wOffset=0 ), testS[1:3,] )
+expect_error(getWindow(testS,wLength=4, wOffset=3 ) )
+expect_error(getWindow(testS,wLength=3, wOffset=-1 ) )
+
+
+#how we cycle through the historic data points
+
+portfolio_timeseries <- function(S, k, wLength){
+  
+  n_rebalance = ceiling((dim(S)[1]-wLength)/k)
+  
+  rebalance_days = wLength+1+(0:(n_rebalance-1))*k
+  
+  for(day in rebalance_days){
+    
+    train_data = getWindow(S, wLength, day-(wLength+1) )
+    
+    #now calculate the portfolio weightings q using train_data. Save these values of q in a list. 
+    
+  }
+  
+  
+}
+
+
+
 
 
 
